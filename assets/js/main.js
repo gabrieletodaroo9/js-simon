cf9d77cedc471e2256b491607dc87c0a46223f61
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-16 16:22:50 +0200

    create macroareas and add basic style to html

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+
+// BONUS:
+// Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.,
+// Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.,
+
+// Aggiungo le variabili che mi servono 
+
+// Aggiungo la funzione dei numeri randomici
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Salvo i 5 numeri dentro una variabile/array
+
+// Visualizzare in pagina i  5 numeri casuali
+
+// Fare partire il timer che fara scomparire i numeri dopo 30 secondi(setTimeout)
+
+// Far apparire in sequenza 5 input
+
+// Prendere i dati forniti dai 5 input 
+
+// Faccio la verifica comparando i numeri
+
+// Creo una funzione per verificare il risultato
+
+// Creo una variabile per i numeri indovinati e una per il punteggio 
+
+// ciclo dentro  ai numeri mandati dall'input
+
+// SE nei numeri generati c'è uno dei numeri che ho scritto
+// -aumento il punteggio 
+// -aggiungo il numero alla variabile dei numeri indovinati
+
+// Stampo nella DOM il risultato in un messaggio con interpolati il punteggio e i numeri indovinati
